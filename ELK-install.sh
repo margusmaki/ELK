049efd2cd2970a16ac37fc880af0ef5a1d08ef72
Margus Potter <margus.potter@gmail.com>
2018-04-06 13:15:06 +0300

    \\\ in nginx

--- ELK-install.sh
+++ ELK-install.sh
@@ -86,7 +86,7 @@ cat <<EOT > /etc/nginx/sites-available/default
 server {
         listen 80;
 	    server_name $eip;
-        return 301 https://\$server_name$\request_uri;
+        return 301 https://\\\$server_name$\request_uri;
 }
 server {
         listen 443 default ssl;
@@ -99,10 +99,10 @@ server {
 # Proxy settings pointing to the Kibana instance
 	    proxy_pass http://localhost:5601/;
 	    proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Upgrade \\\$http_upgrade;
         proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
+        proxy_set_header Host \\\$host;
+        proxy_cache_bypass \\\$http_upgrade;
    }
 }
 EOT
